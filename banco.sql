356c92ea8e6932ea83efaa3c2327c71a2b095c3e
David López <dalo_vidpez@hotmail.com.ar>
2021-09-22 12:24:17 -0300

    Terminada la vista. Ya todo pasa el verificador. Faltaría testear la DB un poco más

--- banco.sql
+++ banco.sql
@@ -251,10 +251,36 @@ CREATE TABLE transferencia(
 
 #--------------------------------------------------
 # CREACIÓN DE VISTAS
-/*
-CREATE VIEW trans_cajas_ahorro AS
-SELECT caja_ahorro.nro_ca, caja_ahorro.saldo, transaccion.nro_trans, transaccion.fecha, transaccion.hora, transaccion.tipo, transaccion.monto
-*/
+
+CREATE VIEW	trans_cajas_ahorro AS
+(/* Consulta para débito */
+SELECT caja_ahorro.nro_ca, caja_ahorro.saldo, transaccion.nro_trans, transaccion.fecha, transaccion.hora, "debito" AS "tipo", transaccion.monto, NULL AS "cod_caja", cliente.nro_cliente, cliente.tipo_doc, cliente.nro_doc, cliente.nombre, cliente.apellido, NULL AS "destino"
+FROM caja_ahorro, transaccion, debito, cliente
+WHERE (transaccion.nro_trans = debito.nro_trans) AND (debito.nro_ca = caja_ahorro.nro_ca) AND (debito.nro_cliente = cliente.nro_cliente)
+)
+UNION
+(
+/* Consulta para extracción */
+SELECT caja_ahorro.nro_ca, caja_ahorro.saldo, transaccion.nro_trans, transaccion.fecha, transaccion.hora, "extraccion" AS "tipo", transaccion.monto, extraccion.nro_ca AS "cod_caja", cliente.nro_cliente, cliente.tipo_doc, cliente.nro_doc, cliente.nombre, cliente.apellido, NULL AS "destino"
+FROM caja_ahorro, transaccion, extraccion, cliente
+WHERE (transaccion.nro_trans = extraccion.nro_trans) AND (extraccion.nro_ca = caja_ahorro.nro_ca) AND (extraccion.nro_cliente = cliente.nro_cliente)
+)
+UNION
+(
+/* Consulta para transferencia */
+SELECT caja_ahorro.nro_ca, caja_ahorro.saldo, transaccion.nro_trans, transaccion.fecha, transaccion.hora, "transferencia" AS "tipo", transaccion.monto, transferencia.origen AS "cod_caja", cliente.nro_cliente, cliente.tipo_doc, cliente.nro_doc, cliente.nombre, cliente.apellido, transferencia.destino
+FROM caja_ahorro, transaccion, transferencia, cliente
+WHERE (transaccion.nro_trans = transferencia.nro_trans) AND (transferencia.origen = caja_ahorro.nro_ca) AND (transferencia.nro_cliente = cliente.nro_cliente)
+)
+UNION
+(
+/* Consulta para depósito */
+SELECT caja_ahorro.nro_ca, caja_ahorro.saldo, transaccion.nro_trans, transaccion.fecha, transaccion.hora, "deposito" AS "tipo", transaccion.monto, deposito.nro_ca AS "cod_caja", cliente.nro_cliente, cliente.tipo_doc, cliente.nro_doc, cliente.nombre, cliente.apellido, NULL AS "destino"
+FROM caja_ahorro, cliente_ca, transaccion, deposito, cliente
+WHERE (transaccion.nro_trans = deposito.nro_trans) AND (deposito.nro_ca = caja_ahorro.nro_ca) AND (caja_ahorro.nro_ca = cliente_ca.nro_ca) AND (cliente_ca.nro_cliente = cliente.nro_cliente)
+);
+
+
 #--------------------------------------------------
 # CREACIÓN DE USUARIOS
 
@@ -278,5 +304,5 @@ GRANT SELECT, INSERT, UPDATE ON banco.cliente TO 'empleado';
 GRANT SELECT, INSERT, UPDATE ON banco.pago TO 'empleado';
 
 CREATE USER 'atm' IDENTIFIED BY 'atm';
---GRANT SELECT ON banco.trans_cajas_ahorro TO 'atm'
+GRANT SELECT ON banco.trans_cajas_ahorro TO 'atm';
 GRANT SELECT, UPDATE ON banco.tarjeta TO 'atm';
--- vista.sql
+++ vista.sql
@@ -1,3 +1,27 @@
 CREATE VIEW	trans_cajas_ahorro AS
-SELECT caja_ahorro.nro_ca, caja_ahorro.saldo, transaccion.nro_trans, transaccion.fecha, transaccion.hora, transaccion.monto, /*caja.cod_caja,*/ cliente.nro_cliente, cliente.tipo_doc, cliente.nro_doc, cliente.nombre, cliente.apellido
-FROM caja_ahorro NATURAL JOIN transaccion NATURAL JOIN cliente;
+(/* Consulta para débito */
+SELECT caja_ahorro.nro_ca, caja_ahorro.saldo, transaccion.nro_trans, transaccion.fecha, transaccion.hora, "debito" AS "tipo", transaccion.monto, NULL AS "cod_caja", cliente.nro_cliente, cliente.tipo_doc, cliente.nro_doc, cliente.nombre, cliente.apellido, NULL AS "destino"
+FROM caja_ahorro, transaccion, debito, cliente
+WHERE (transaccion.nro_trans = debito.nro_trans) AND (debito.nro_ca = caja_ahorro.nro_ca) AND (debito.nro_cliente = cliente.nro_cliente)
+)
+UNION
+(
+/* Consulta para extracción */
+SELECT caja_ahorro.nro_ca, caja_ahorro.saldo, transaccion.nro_trans, transaccion.fecha, transaccion.hora, "extraccion" AS "tipo", transaccion.monto, extraccion.nro_ca AS "cod_caja", cliente.nro_cliente, cliente.tipo_doc, cliente.nro_doc, cliente.nombre, cliente.apellido, NULL AS "destino"
+FROM caja_ahorro, transaccion, extraccion, cliente
+WHERE (transaccion.nro_trans = extraccion.nro_trans) AND (extraccion.nro_ca = caja_ahorro.nro_ca) AND (extraccion.nro_cliente = cliente.nro_cliente)
+)
+UNION
+(
+/* Consulta para transferencia */
+SELECT caja_ahorro.nro_ca, caja_ahorro.saldo, transaccion.nro_trans, transaccion.fecha, transaccion.hora, "transferencia" AS "tipo", transaccion.monto, transferencia.origen AS "cod_caja", cliente.nro_cliente, cliente.tipo_doc, cliente.nro_doc, cliente.nombre, cliente.apellido, transferencia.destino
+FROM caja_ahorro, transaccion, transferencia, cliente
+WHERE (transaccion.nro_trans = transferencia.nro_trans) AND (transferencia.origen = caja_ahorro.nro_ca) AND (transferencia.nro_cliente = cliente.nro_cliente)
+)
+UNION
+(
+/* Consulta para depósito */
+SELECT caja_ahorro.nro_ca, caja_ahorro.saldo, transaccion.nro_trans, transaccion.fecha, transaccion.hora, "deposito" AS "tipo", transaccion.monto, deposito.nro_ca AS "cod_caja", cliente.nro_cliente, cliente.tipo_doc, cliente.nro_doc, cliente.nombre, cliente.apellido, NULL AS "destino"
+FROM caja_ahorro, cliente_ca, transaccion, deposito, cliente
+WHERE (transaccion.nro_trans = deposito.nro_trans) AND (deposito.nro_ca = caja_ahorro.nro_ca) AND (caja_ahorro.nro_ca = cliente_ca.nro_ca) AND (cliente_ca.nro_cliente = cliente.nro_cliente)
+);
